d192215ceac64d28a7ae1bbd26e43033d1aa5243
= <jishuk212@gmail.com>
2026-09-03 13:02:23 +0530

    Update frontend api.js file remove header

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://authentication-project-1-apyd.onrender.com/api",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
